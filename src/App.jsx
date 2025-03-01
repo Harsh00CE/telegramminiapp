374c77cb6703e65408907f95d9f2a6509e271d11
Harsh <140328237+Harsh0R@users.noreply.github.com>
2025-03-01 15:47:57 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
 import Herosection from "./components/Herosection/Herosection";
 import Footer from "./components/Footer/Footer";
 import Mybank from "./Pages/Mybank/Mybank";
 import Myteam from "./Pages/Myteam/Myteam";
 import EnergyPage from "./Pages/EnergyPage/EnergyPage";
 import EnergyStakingPage from "./Pages/EnergyStakingPage/EnergyStakingPage";
+import { backButton } from "@telegram-apps/sdk";
 
 function App() {
   const [tg, setTg] = useState(null);
@@ -27,13 +28,31 @@ function App() {
     <Router> {/* Ensure everything is inside Router */}
       <Routes>
         <Route path="/" element={<Herosection username={username} />} />
-        <Route path="/mybank" element={<Mybank />} />
-        <Route path="/myteam" element={<Myteam />} />
-        <Route path="/energystaking" element={<EnergyStakingPage />} />
-        <Route path="/energy" element={<EnergyPage />} />
+        <Route path="/energy" element={<PageWithBackButton Component={EnergyPage} />} />
+        <Route path="/mybank" element={<PageWithBackButton Component={Mybank} />} />
+        <Route path="/myteam" element={<PageWithBackButton Component={Myteam} />} />
+        <Route path="/energystaking" element={<PageWithBackButton Component={EnergyStakingPage} />} />
       </Routes>
     </Router>
   );
 }
+
+const PageWithBackButton = ({ Component }) => {
+  const navigate = useNavigate();
+  const backButton = backButton();
+
+  useEffect(() => {
+    backButton.show();
+    backButton.onClick(() => {
+      navigate(-1);
+    });
+
+    return () => {
+      backButton.hide();
+    };
+  }, [backButton, navigate]);
+
+  return <Component />;
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { init, backButton } from '@telegram-apps/sdk-react';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+import App from './App';
+import { BackButton } from './utils/BackButton';
+
+init();
+
+backButton.mount();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <>
+    <BackButton />
     <App />
-  </StrictMode>,
-)
+  </>
+);
