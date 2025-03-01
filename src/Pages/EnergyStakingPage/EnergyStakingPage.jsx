21f991cb84ba99923a07aecdfb0b6a5dbd2d3d86
Harsh <140328237+Harsh0R@users.noreply.github.com>
2025-03-01 15:33:07 +0530

    push

--- src/Pages/EnergyStakingPage/EnergyStakingPage.jsx
+++ src/Pages/EnergyStakingPage/EnergyStakingPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const EnergyStakingPage = () => {
-  return (
-    <div>EnergyStakingPage</div>
-  )
+    return (
+        <div className="w-full flex justify-between items-center p-4 bg-gray-900 rounded-lg">
+            <span>Energy Staking</span>
+            <div className="flex gap-2">
+                <button className="bg-yellow-500 text-black px-4 py-1 rounded">Referral Link</button>
+                <button className="bg-yellow-500 text-black px-4 py-1 rounded">Upgrade</button>
+            </div>
+        </div>
+    )
 }
 
 export default EnergyStakingPage
--- src/Pages/Myteam/Myteam.jsx
+++ src/Pages/Myteam/Myteam.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const Myteam = () => {
-  return (
-    <div>Myteam</div>
-  )
+    return (
+        <div className="w-full flex justify-between items-center p-4 bg-gray-900 rounded-lg">
+            <span>My Team</span>
+            <div className="flex gap-2">
+                <button className="bg-yellow-500 text-black px-4 py-1 rounded">Referral Link</button>
+                <button className="bg-yellow-500 text-black px-4 py-1 rounded">Upgrade</button>
+            </div>
+        </div>
+    )
 }
 
 export default Myteam
