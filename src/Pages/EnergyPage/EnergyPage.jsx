eca8059bc6e4e125fa210277e019d4fbee50cac9
Harsh <140328237+Harsh0R@users.noreply.github.com>
2025-03-01 15:57:03 +0530

    push

--- src/Pages/EnergyPage/EnergyPage.jsx
+++ src/Pages/EnergyPage/EnergyPage.jsx
@@ -8,7 +8,13 @@ const EnergyPage = () => {
   });
 
   return (
-    <div>Get EnergyPage</div>
+    <div className="w-full flex justify-between items-center p-4 bg-gray-900 rounded-lg">
+      <span>Get Energy</span>
+      <div className="flex gap-2">
+        <button className="bg-yellow-500 text-black px-4 py-1 rounded">Referral Link</button>
+        <button className="bg-yellow-500 text-black px-4 py-1 rounded">Upgrade</button>
+      </div>
+    </div>
   )
 }
 
