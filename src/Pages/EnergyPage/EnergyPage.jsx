06bf0075047bca1f89339e3230351a0b13129e07
Harsh <140328237+Harsh0R@users.noreply.github.com>
2025-03-03 13:05:06 +0530

    backburtton update

--- src/Pages/EnergyPage/EnergyPage.jsx
+++ src/Pages/EnergyPage/EnergyPage.jsx
@@ -2,11 +2,6 @@ import React from 'react'
 
 const EnergyPage = () => {
 
-  window.Telegram.WebApp.BackButton.onClick(() => {
-    alert("Navigated back!");
-    history.back();
-  });
-
   return (
     <div className="w-full flex justify-between items-center p-4 bg-gray-900 rounded-lg">
       <span>Get Energy</span>
