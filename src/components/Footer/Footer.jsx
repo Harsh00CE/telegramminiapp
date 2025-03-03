e1d55501d875942c43c8afd8b1040c7288952f1b
Harsh <140328237+Harsh0R@users.noreply.github.com>
2025-03-03 15:09:33 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,8 +5,7 @@ const Footer = () => {
     const navigate = useNavigate();
 
     return (
-        <div className="fixed bottom-0 left-0 right-0 flex justify-around py-2 border-t border-gray-700 text-white"
-            style={{ background: 'rgba(29, 32, 37, 0.8)' }}>
+        <div className="flex justify-around py-2 border-t border-gray-700 text-white">
             <button onClick={() => navigate("/mybank")} className="flex flex-col items-center">
                 <span className="text-2xl">🏦</span>
                 <p>MY BANK</p>
