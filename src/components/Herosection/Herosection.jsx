044d39571353d04ab4649e45e600fc655b56e249
Harsh <203636452+Harsh00CE@users.noreply.github.com>
2025-04-15 15:01:54 +0530

    push

--- src/components/Herosection/Herosection.jsx
+++ src/components/Herosection/Herosection.jsx
@@ -60,7 +60,7 @@ const Herosection = ({ username }) => {
             <div className="w-full flex justify-between items-center p-4 bg-gray-900 rounded-lg">
                 <span>Hello , {username}</span>
                 <div className="flex gap-2">
-                    <button className="bg-yellow-500 text-black px-4 py-1 rounded">Referral Link</button>
+                    <button className="bg-yellow-500 text-black px-4 py-1 rounded">Referral1324655 Link</button>
                     <button className="bg-yellow-500 text-black px-4 py-1 rounded">Upgrade</button>
                 </div>
             </div>
